b9b99a32c7288f5634340901393702cca81ef46e
chabat <felipechabat@gmail.com>
2017-02-15 07:45:29 -0200

    Some tests made

---
+++ teste/index.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+		<title> Isso é um teste </title>
+		<link type="text/css" rel = "stylesheet" href = "css/index.css">
+	</head>
+
+	<body>
+		<h1> Testando um negócio aqui </h1>
+		<h3> Menorzim </h3>
+		<p> <!-- Começo do paragrafo -->
+			<strong> forte </strong>
+			<small> pequeno </small>
+			<em> italico </em>
+		</p>
+		<figure> <!-- figura do cão co alho na cabeça -->
+			<img src = "img/dogarlic.jpg" alt = "Cachorro com alho na cabeça"
+				width = "304" height = "228">
+			<figcaption> Cachorro com alho na cabeça </figcaption>
+		</figure>
+
+		<ul> <!-- Lista desordenada -->
+			<li> Primeiro item da lista
+			<li> Segundo item da lista
+			<li> Terceiro item da lista </li>
+		</ul>
+
+		<ol> <!-- Lista ordenada -->
+			<li>Primeiro item da lista</li>
+			<li>Segundo item da lista</li>
+			<li>Terceiro item da lista</li>
+			<li>Quarto item da lista</li>
+			<li>Quinto item da lista</li>
+		</ol>
+
+		<dl> <!-- Lista de definições -->
+			<dt>HTML</dt>
+			<dd>
+				HTML é a linguagem de marcação de textos utilizada
+				para exibir textos como páginas na Internet.
+			</dd>
+			<dt>Navegador</dt>
+			<dd>
+				Navegador é o software que requisita um documento HTML
+				através do protocolo HTTP e exibe seu conteúdo em uma
+				janela.
+			</dd>
+		</dl>
+		<!--Links dentro do HTML -->
+		<p>Clicando no <a href="index.php">menu</a> você acessa nosso menu.</p>
+		<ul>
+			<li>Menu <a href="index.php">inicial</a></li>
+			<li> <a href= "https://www.google.com.br"> Google</a></li>
+		</ul>
+		<!-- BLABLABALBLABLABLABLABLA -->
+	</body>
+</html>
